dfb15e81848da693978aeae8e4ecf86d94298d6f
Destiny OU <you@example.com>
2020-08-14 11:52:55 +0800

    SHYT

---
+++ shyt_app/src/views/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>新组件模板</h2>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="box">
+      hahah
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+<style lang="scss" scoped>
+.box {
+  width: 75px;
+  height: 75px;
+  background: $themeColor;
+}
+</style>
